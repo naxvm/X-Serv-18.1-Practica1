d9b5aa695d2a3945c0a73e50ab60ab5f6d051066
Nacho Condés <nachocmstrato@gmail.com>
2017-03-10 14:00:37 +0100

    Terminado. Falta pulir

--- practica1.py
+++ practica1.py
@@ -1,24 +1,55 @@
 #! /usr/bin/python3
 
 import webapp
+import csv
 
 class shortenApp(webapp.webApp):
 
-    shortenedURLs = {}
-    longURLs = {} # Almaceno como clave el nombre de recurso
+    def initializeDicts(self):
+        # primero, leo el diccionario existente, y lo importo a las listas
+        with open(self.filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.shortenedURLs[row['larga']] = row['corta']
+                print(row['larga'], row['corta'])
+                self.longURLs[row['corta']] = row['larga']
+
+    def updateDicts(self,longURL,shortURL):
+        #shortURL = ('http://localhost:1324' + shortURL)
+        self.shortenedURLs[longURL] = shortURL
+        self.longURLs[shortURL] = longURL # actualizo los diccionarios
+
+        with open(self.filename, 'a') as csvfile:    # actualizo el csv
+            fieldnames = ['larga', 'corta']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'larga': longURL, 'corta': shortURL})
 
 
-    form = ('<form method="post">' +
+    def __init__(self, hostname, port, filename):
+
+        self.shortenedURLs = {}
+        self.longURLs = {} # Almaceno como clave el nombre de recurso
+
+        self.filename = filename
+
+        self.form = ('<form method="post">' +
             'Introduce tu URL:<br>' +
             '<input type="text" name="url">' +
             '<input type="submit" value="Acortar">' +
             '</form>') # El navegador dará los datos por POST a la misma página
 
 
-    notFound = '<html><head><b>Error</b><br>Recurso no disponible</head></html>'
+        self.notFound = '<html><head><b>Error</b><br>Recurso no disponible</head></html>'
 
-    lastIndex = 0
+        self.emptyRequest = ('<html><head><b>Error</b><br>Has enviado una URL vacia.<br>' +
+                    '<a href="http://localhost:1234">Vuelve a intentarlo</a></head></html>')
 
+        self.initializeDicts()
+
+        self.lastIndex = len(self.shortenedURLs) # para no machacar las ya existentes
+
+        webapp.webApp.__init__(self, hostname, port) # método init de la clase antigua
+        # tiene que ir lo último porque ya pone el servidor en marcha
 
     def parse(self, request):
             # De momento devolverá el método y el recurso que se piden
@@ -57,6 +88,10 @@ class shortenApp(webapp.webApp):
                     responseBody = responseBody + '</html>'
 
             else:   # si me piden una URL acortada
+                resource = ('http://localhost:1234' + resource)
+                print('Voy a buscar ', resource)
+                print('Y el diccionario está así: ')
+                print(self.longURLs)
                 if resource in self.longURLs: # si la acortada está en la lista:
                     returnCode = ('301 Moved Permanently\r\n'
                     + 'Location: ' + self.longURLs[resource])
@@ -70,6 +105,9 @@ class shortenApp(webapp.webApp):
             if newOne == None:
                 returnCode = '404 Not Found'
                 responseBody = self.notFound
+            elif len(newOne) == 0:
+                returnCode = '404 Not Found'
+                responseBody = self.emptyRequest
             else:
                 if newOne[0:7] != 'http://' and newOne[0:8] != 'https://':
                     # la página está mal pasada
@@ -78,13 +116,23 @@ class shortenApp(webapp.webApp):
                 if newOne in self.shortenedURLs:
                     newIndex = self.shortenedURLs[newOne] # la página ya existía. La vuelvo a servir
                 else:
-                    newURL = ('/' + str(self.lastIndex)) # conversión para tener la barra
+                    newURL = ('http://localhost:1234/' + str(self.lastIndex)) # conversión para tener la barra
                     self.lastIndex = self.lastIndex + 1
 
+                    self.updateDicts(newOne, newURL)
+                    print('Ahora los diccionarios son: \nlongURLs:')
+                    print(self.longURLs)
+                    print('\nshortenedURLs:')
+                    print(self.shortenedURLs)
+                    '''
                     self.shortenedURLs[newOne] = ('http://localhost:1234' + newURL)
                     self.longURLs[newURL] = newOne
-
+                    '''
                     returnCode = '200 OK';
+                    print('============================')
+                    print('Voy a buscar en longURLs: ', newURL)
+                    print('Voy a buscar en shortenedURLs: ', newOne)
+
                     responseBody = ('<html><head>Acortamiento realizado<br></head><body>' +
                     '<a href="' + self.longURLs[newURL] +'">URL larga</a><br>' +
                     '<a href="' + self.shortenedURLs[newOne] + '">URL corta</a>' + '</body></html>')
@@ -106,4 +154,4 @@ class shortenApp(webapp.webApp):
 
 
 if __name__ == '__main__':
-    app = shortenApp('localhost',1234)
+    app = shortenApp('localhost',1234, filename='direcciones.csv')
